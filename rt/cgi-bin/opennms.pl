5198f1177a715788569a2b7c15b9756544742c2c
Jonathan Sartin <jsartin@users.sourceforge.net>
2008-08-01 01:09:17 +0000

    in goes the cgi

---
+++ rt/cgi-bin/opennms.pl
@@ -0,0 +1,26 @@
+#!/opt/local/bin/perl -w
+# --
+# bin/cgi-bin/opennms.pl - Dispatcher script for OpenNMS Integration module
+# Copyright (C) (Jonathan Sartin) (Jonathan@opennms.org)
+# --
+# $Id: opennms.pl 18 2008-05-08 17:59:48Z user $
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl-2.0.txt.
+# --
+ 
+use strict;
+use warnings;
+
+# use ../../ as lib location
+# This is OTRS specific and will need changing.
+use FindBin qw($Bin);
+use lib "$Bin/../..";
+use lib "$Bin/../../Kernel/cpan-lib";
+ 
+use SOAP::Transport::HTTP;
+
+my $ClassMap = {'http://opennms.org/integration/rt/ticketservice' => 'opennms::TicketService'};
+
+SOAP::Transport::HTTP::CGI->dispatch_with($ClassMap)->handle;
